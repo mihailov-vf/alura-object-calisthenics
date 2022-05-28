74ffa282cfa5815281e9ffb4acc859434e480dde
Mihailov Vasilievic Filho <mihailov.vf@gmail.com>
2022-05-28 14:02:02 -0300

    Aula 2 - Reduzindo a complexidade

--- Anotações.md
+++ Anotações.md
@@ -2,6 +2,12 @@
 
 ### Object Calistenics
 1. Evitar uso de getters e setters
-  1.1. Tell, don't ask
-    - Não expor o estado interno do objeto.
-    - Expor apenas o comportamento.
+    1. Tell, don't ask
+        - Não expor o estado interno do objeto.
+        - Expor apenas o comportamento.
+2. Reduzir a [Complexidade cognitiva](https://www.c-sharpcorner.com/blogs/cognitive-complexity-vs-cyclomatic-complexity-an-example-with-c-sharp)
+    1. Ter apenas um nível de identação por método
+        - Extrair lógicas complexas para outros métodos
+    2. Não utilizar else
+        - Early return
+        - Fail fast
--- src/Domain/Student/Student.php
+++ src/Domain/Student/Student.php
@@ -66,19 +66,20 @@ class Student
 
     public function hasAccess(): bool
     {
-        if ($this->watchedVideos->count() > 0) {
-            $this->watchedVideos->sort(fn (DateTimeInterface $dateA, DateTimeInterface $dateB) => $dateA <=> $dateB);
-            /** @var DateTimeInterface $firstDate */
-            $firstDate = $this->watchedVideos->first()->value;
-            $today = new \DateTimeImmutable();
-
-            if ($firstDate->diff($today)->days >= 90) {
-                return false;
-            } else {
-                return true;
-            }
-        } else {
+        if ($this->watchedVideos->isEmpty()) {
             return true;
         }
+
+        return $this->firstVideoWatchedWithin90Days();
+    }
+
+    private function firstVideoWatchedWithin90Days(): bool
+    {
+        $this->watchedVideos->sort(fn (DateTimeInterface $dateA, DateTimeInterface $dateB) => $dateA <=> $dateB);
+        /** @var DateTimeInterface $firstDate */
+        $firstDate = $this->watchedVideos->first()->value;
+        $today = new \DateTimeImmutable();
+
+        return $firstDate->diff($today)->days < 90;
     }
 }
